bba5feec296a12037ef6818c63b8b379ba9e2196
ikukadia <kukadia2@illinois.edu>
2019-04-11 18:09:56 -0500

    init commit

---
+++ utils.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(self, name, profile_picture):
+        self.name = name
+        self.profile_picture = profile_picture
+
+class React:
+    def __init__(self, name, actor, content):
+        self.name = name
+        self.actor = actor
+        self.content = content
+        
