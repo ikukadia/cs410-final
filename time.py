ff9b5ea9f7efdb43fb81042a6676e40d7c796cc0
suhirtha99 <sraj6@illinois.edu>
2019-04-12 01:28:54 -0500

    early bird, night owl in progress

---
+++ time.py
@@ -0,0 +1,25 @@
+import datetime
+import io
+import json
+from utils import get_participants, get_messages
+
+def time_range_analysis(data, start, end):
+    num_messages = {}
+    for user in get_participants(data):
+        num_messages[user] = 0
+        for message in get_messages(data, user):
+            hour = datetime.datetime.fromtimestamp(message["timestamp_ms"]/1000.0).strftime('%Y-%m-%d %H:%M:%S.%f').split()[1].split(":")[0]
+            if int(hour) >= start and int(hour) <= (end - 1):
+                num_messages[user] += 1
+    return num_messages
+
+def init():
+    with open('dont_commit.json') as file:
+        data = json.load(file)
+    return data
+
+
+
+
+
+
