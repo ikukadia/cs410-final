6fc2e5e4c1a8eb4d830ace5ac68eadfbc564913a
Sarah Nadeem <snadeem2@illinois.edu>
2019-04-14 15:59:41 -0500

    main function to call all files

---
+++ Main.py
@@ -0,0 +1,20 @@
+import changes
+import mentioned
+import poll
+import react
+import time
+import uniqueWords
+import words
+
+
+def main():
+    changes.main()
+    mentioned.main()
+    poll.main()
+    react.main()
+    #time.init()
+    uniqueWords.main()
+    #words.main()
+
+if __name__ == '__main__':
+    main()
